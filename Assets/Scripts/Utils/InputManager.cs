23fbee4b3d9a02a158496e705f54827e2ba626a7
Takayuki Cho <taka.3365@gmail.com>
2019-09-17 22:29:06 +0900

    add codes

---
+++ Assets/Scripts/Utils/InputManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+    None,
+}
+
+public class InputManager
+{
+    public static Direction getDirection(){
+        var vertical = (int) Input.GetAxisRaw("Vertical") ;
+        var horizontal = (int) Input.GetAxisRaw("Horizontal");
+
+        if(vertical > 0){
+            return Direction.Up;
+        }else if (vertical < 0) {
+            return Direction.Down;
+        }else if (horizontal > 0) {
+            return Direction.Right;
+        }else if (horizontal < 0 ){
+            return Direction.Left;
+        }
+        return Direction.None;
+    }
+}
