f76b672d1aeeac6084009acda3b31acfc739e74c
KONDO-yuuki <efb.mzw.yk@gmail.com>
2019-10-01 21:48:15 +0900

    ゲーム開始時にカードを引けるようにした

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class GameController : MonoBehaviour, GameControllable
 {
     // Start is called before the first frame update
+    public GameObject canvas;
     int life;
     public UnityEngine.UI.Text lifeLabel;
+    int handCount;
 
     void Start()
     {
-        life = 20;
+        this.life = 20;
+        this.handCount = 0;
         lifeLabel.text = life.ToString();
+        for (int i = 0; i <= 5; i++)
+        {
+            DrawCard();
+        }
 
     }
     public void LifeChange(int num)
@@ -25,5 +32,18 @@ public class GameController : MonoBehaviour, GameControllable
     public void CastCard(CardImpl impl)
     {
         impl.Cast(this);
+    }
+
+    private void SetUpTable()
+    {
+        DrawCard();
+    }
+    private void DrawCard()
+    {
+        this.handCount += 1;
+        var interval = handCount * 100;
+        GameObject castableCardPrefab = Instantiate((GameObject)Resources.Load("CastableCard"));
+        castableCardPrefab.transform.SetParent(canvas.transform, false);
+        castableCardPrefab.GetComponent<RectTransform>().anchoredPosition = new Vector2(-300 + interval, -205);
     }
 }
