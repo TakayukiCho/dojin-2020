46a53e5bc09ffa54cbd2199cb4b01247f8e8fc37
KONDO-yuuki <efb.mzw.yk@gmail.com>
2019-11-05 20:44:46 +0900

    minimumなTurnMediatorのテストとその実行環境を作成

---
+++ Assets/Scripts/Tests/Editor/TurnMediatorTestTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Tests
+{
+    public class TurnMediatorTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TurnMediator()
+        {
+            var med = new TurnMediator();
+            Assert.AreEqual(1, med.DrawCard());
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator GameControllerTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/TurnMediator.cs
@@ -0,0 +1,13 @@
+﻿public class TurnMediator
+{
+
+    // ゲーム開始時の引数をコンストラクタで処理する
+    public TurnMediator()
+    {
+
+    }
+    public int DrawCard()
+    {
+        return 1;
+    }
+}
