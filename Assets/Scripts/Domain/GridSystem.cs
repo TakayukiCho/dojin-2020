b2a49b688747e111325b7c5a7c2c0e9d2e645065
KONDO-yuuki <efb.mzw.yk@gmail.com>
2019-12-02 19:35:20 +0900

    gridsystemをsignleton化した

--- Assets/Scripts/Domain/GridSystem.cs
+++ Assets/Scripts/Domain/GridSystem.cs
@@ -2,7 +2,26 @@ using System.Linq;
 public class GridSystem
 {
     Coordinates[,] tiles;
-    GridSystem(int column, int row)
+    static private GridSystem gridSystem;
+    static public GridSystem Init(int column, int row)
+    {
+        if (gridSystem != null)
+        {
+            throw new System.Exception("girdsytem already defined");
+        }
+        new GridSystem(column, row);
+        return gridSystem;
+    }
+
+    static public GridSystem GetInstance()
+    {
+        if (gridSystem == null)
+        {
+            throw new System.Exception("girdsytem shoudl be init");
+        }
+        return gridSystem;
+    }
+    private GridSystem(int column, int row)
     {
         tiles = new Coordinates[column, row];
         var sequence = from x in Enumerable.Range(0, column)
@@ -12,5 +31,7 @@ public class GridSystem
         {
             tiles[index.x, index.y] = new Coordinates();
         }
+        gridSystem = this;
     }
 }
+
